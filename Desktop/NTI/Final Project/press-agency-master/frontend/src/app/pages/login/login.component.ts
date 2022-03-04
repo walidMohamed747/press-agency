f77773f3e87efbdfb35a119809cbf4dfa931cdc3
walidMohamed747 <84786097+walidMohamed747@users.noreply.github.com>
2022-03-04 18:30:57 +0200

    my changes

--- Desktop/NTI/Final Project/press-agency-master/frontend/src/app/pages/login/login.component.ts
+++ Desktop/NTI/Final Project/press-agency-master/frontend/src/app/pages/login/login.component.ts
@@ -13,8 +13,8 @@ export class LoginComponent implements OnInit {
   msg=""
   isSubmitted = false
   loginForm: FormGroup = new FormGroup({
-    email: new FormControl('marwa1@test.com', [Validators.email, Validators.required]),
-    password:new FormControl('123456', [Validators.required,Validators.minLength(3), Validators.maxLength(20)])
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password:new FormControl('', [Validators.required,Validators.minLength(3), Validators.maxLength(20)])
   })
   get email(){return this.loginForm.get('email')}
   get password(){return this.loginForm.get('password')}
--- Desktop/NTI/Final Project/press-agency-master/frontend/src/app/pages/register/register.component.ts
+++ Desktop/NTI/Final Project/press-agency-master/frontend/src/app/pages/register/register.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/providers/auth.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -7,20 +8,34 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+msg =''
+isSubmitted =false 
   registerForm : FormGroup = new FormGroup ({
-    firstName : new FormControl(['' , Validators.required]),
-    lastName : new FormControl(['', Validators.required]),
-    email : new FormControl([Validators.email , Validators.required]),
-    password : new FormControl([Validators.minLength(3)]), 
-    birthDate : new FormControl(['',Validators.required]), 
-    userRole :new FormControl(['',Validators.required ]) 
+    firstName : new FormControl('',[ Validators.required]),
+    lastName : new FormControl('',[ Validators.required]),
+    email : new FormControl('',[Validators.email , Validators.required]),
+    password : new FormControl('',[Validators.minLength(3)]), 
+    birthDate : new FormControl('',[Validators.required]), 
+    userRole :new FormControl('',[Validators.required ]) 
   })
-  constructor( private _http : AuthService) { }
+  constructor( private _auth : AuthService,  private _router : Router ) { }
 
   ngOnInit(): void {
   }
 register(){
-  
+  this.isSubmitted=true
+  if(this.registerForm.valid){
+    this._auth.register(this.registerForm.value).subscribe((data)=>
+    {console.log(data)},(err)=>{this.msg="Unauthorized"},
+    )
+  }
+}
+//function to redirect to viwer page //
+isViwier(){
+
+}
+//function to redirect to editor page //
+isEditor(){
+
 }
 }
